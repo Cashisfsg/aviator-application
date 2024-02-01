1892a597b0249db215644777cb55dc979982efa3
Cashisfsg <cashisfsg@gmail.com>
2024-02-01 14:00:40 +0200

    add top bets for unauthorized user

--- src/containers/statistics.tsx
+++ src/containers/statistics.tsx
@@ -10,45 +10,39 @@ export const Statistics = () => {
 
     return (
         <>
-            {isAuthenticated ? (
-                <Tabs
-                    defaultValue="all"
-                    className="rounded-2.5xl pb-8"
-                >
-                    <TabsList>
-                        <TabsTrigger value="all">Все ставки</TabsTrigger>
+            <Tabs
+                defaultValue="all"
+                className="rounded-2.5xl pb-8"
+            >
+                <TabsList>
+                    <TabsTrigger value="all">Все ставки</TabsTrigger>
+                    {isAuthenticated ? (
                         <TabsTrigger value="my">Мои</TabsTrigger>
-                        <TabsTrigger value="top">Топ</TabsTrigger>
-                    </TabsList>
-                    <TabsContent
-                        value="all"
-                        className="mt-5 gap-x-1 gap-y-2 rounded-2.5xl bg-black-50 px-1.5 py-5"
-                    >
-                        <AllBetsTabpanel />
-                    </TabsContent>
+                    ) : null}
+
+                    <TabsTrigger value="top">Топ</TabsTrigger>
+                </TabsList>
+                <TabsContent
+                    value="all"
+                    className="mt-5 gap-x-1 gap-y-2 rounded-2.5xl bg-black-50 px-1.5 py-5"
+                >
+                    <AllBetsTabpanel />
+                </TabsContent>
+                {isAuthenticated ? (
                     <TabsContent
                         value="my"
                         className="mt-5 gap-x-1 gap-y-2 overflow-hidden rounded-2.5xl bg-black-50 pb-5 text-lg"
                     >
                         <MyBetsTabpanel />
                     </TabsContent>
-                    <TabsContent
-                        value="top"
-                        className="mt-5 gap-x-1 gap-y-2 overflow-hidden rounded-2.5xl bg-black-50 pb-5 text-lg"
-                    >
-                        <TopBetsTabpanel />
-                    </TabsContent>
-                </Tabs>
-            ) : (
-                <section className="overflow-hidden rounded-2.5xl bg-black-50">
-                    <h2 className="rounded-md bg-[#2c2d30] px-4 py-2 text-lg font-bold text-gray-300">
-                        Игровая статистика
-                    </h2>
-                    <div className="px-1.5 py-5">
-                        <AllBetsTabpanel />
-                    </div>
-                </section>
-            )}
+                ) : null}
+                <TabsContent
+                    value="top"
+                    className="mt-5 gap-x-1 gap-y-2 overflow-hidden rounded-2.5xl bg-black-50 pb-5 text-lg"
+                >
+                    <TopBetsTabpanel />
+                </TabsContent>
+            </Tabs>
         </>
     );
 };
