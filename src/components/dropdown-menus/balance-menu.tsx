921e91ef5d78a1064d65bb3ebfa7350143ea8893
Cashisfsg <cashisfsg@gmail.com>
2024-02-08 22:49:41 +0200

    fix balance menu for firefox focus

--- src/components/dropdown-menus/balance-menu.tsx
+++ src/components/dropdown-menus/balance-menu.tsx
@@ -1,50 +1,196 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "@/store/hooks/useAuth";
 import { useGetUserBalanceQuery } from "@/store";
 
+// import {
+//     DropdownMenu,
+//     DropdownMenuContent,
+//     DropdownMenuItem,
+//     DropdownMenuLabel,
+//     DropdownMenuSeparator,
+//     DropdownMenuTrigger
+// } from "@/components/ui/dropdown-menu";
+
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu";
+    Popover,
+    PopoverTrigger,
+    PopoverContent
+} from "@/components/ui/popover";
+
+import {
+    DropDownMenuItem,
+    DropDownMenuSeparator
+} from "@/components/ui/drop-down-list";
+
+// export const BalanceMenu = () => {
+//     const { isAuthenticated } = useAuth();
+
+//     const { data: balance } = useGetUserBalanceQuery(undefined, {
+//         skip: !isAuthenticated
+//     });
+
+//     return (
+//         <>
+//             {isAuthenticated ? (
+//                 <DropdownMenu modal={false}>
+//                     <DropdownMenuTrigger
+//                         disabled={!isAuthenticated}
+//                         className="rounded-full border border-[#414148] bg-[#252528] px-3 py-0.5"
+//                     >
+//                         {balance?.balance.toFixed(2) || "300"}{" "}
+//                         {balance?.currency || "$"}
+//                     </DropdownMenuTrigger>
+//                     <DropdownMenuContent forceMount>
+//                         <DropdownMenuLabel>
+//                             Баланс {balance?.balance.toFixed(2)}{" "}
+//                             {balance?.currency}
+//                         </DropdownMenuLabel>
+//                         <DropdownMenuSeparator />
+//                         <DropdownMenuItem asChild>
+//                             <Link to="/payment/replenishment">Пополнить</Link>
+//                         </DropdownMenuItem>
+//                         <DropdownMenuSeparator />
+//                         <DropdownMenuItem asChild>
+//                             <Link to="/payment/draw">Вывести</Link>
+//                         </DropdownMenuItem>
+//                     </DropdownMenuContent>
+//                 </DropdownMenu>
+//             ) : null}
+//         </>
+//     );
+// };
 
 export const BalanceMenu = () => {
+    const [open, setOpen] = useState(false);
     const { isAuthenticated } = useAuth();
 
     const { data: balance } = useGetUserBalanceQuery(undefined, {
         skip: !isAuthenticated
     });
 
+    const onClickHandler = () => {
+        setOpen(false);
+    };
+
     return (
-        <>
+        <Popover
+            open={open}
+            onOpenChange={setOpen}
+        >
+            <PopoverTrigger className="rounded-full border border-[#414148] bg-[#252528] px-3 py-0.5">
+                {balance?.balance.toFixed(2) || "300"}{" "}
+                {balance?.currency || "$"}
+            </PopoverTrigger>
+
             {isAuthenticated ? (
-                <DropdownMenu modal={false}>
-                    <DropdownMenuTrigger
-                        disabled={!isAuthenticated}
-                        className="rounded-full border border-[#414148] bg-[#252528] px-3 py-0.5"
-                    >
-                        {balance?.balance.toFixed(2) || "300"}{" "}
-                        {balance?.currency || "$"}
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>
+                <PopoverContent
+                    side="bottom"
+                    sideOffset={6}
+                    align="center"
+                    className="w-auto min-w-[8rem] border-none bg-[#252528] p-0 shadow-md"
+                >
+                    <ul className="select-none text-left text-sm text-white">
+                        <li className="w-max bg-[#2c2d30] px-2.5 py-2 font-semibold">
                             Баланс {balance?.balance.toFixed(2)}{" "}
                             {balance?.currency}
-                        </DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem asChild>
-                            <Link to="/payment/replenishment">Пополнить</Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem asChild>
-                            <Link to="/payment/draw">Вывести</Link>
-                        </DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                        </li>
+
+                        <DropDownMenuSeparator />
+
+                        <DropDownMenuItem onClick={onClickHandler}>
+                            <Link
+                                to="/payment/replenishment"
+                                className="inline-block w-full px-2.5 py-2"
+                            >
+                                Пополнить
+                            </Link>
+                        </DropDownMenuItem>
+
+                        <DropDownMenuSeparator />
+
+                        <DropDownMenuItem onClick={onClickHandler}>
+                            <Link
+                                to="/payment/draw"
+                                className="inline-block w-full px-2.5 py-2"
+                            >
+                                Вывести
+                            </Link>
+                        </DropDownMenuItem>
+                    </ul>
+                </PopoverContent>
             ) : null}
-        </>
+        </Popover>
     );
 };
+
+// export const BalanceMenu = () => {
+//     const { isAuthenticated } = useAuth();
+
+//     const { data: balance } = useGetUserBalanceQuery(undefined, {
+//         skip: !isAuthenticated
+//     });
+
+//     const dialogRef = useRef<HTMLDialogElement>(null);
+
+//     return (
+//         <div className="relative">
+//             <button
+//                 onClick={() => {
+//                     const open = dialogRef.current?.hasAttribute("open");
+
+//                     if (open) {
+//                         dialogRef.current?.close();
+//                         return;
+//                     }
+
+//                     dialogRef.current?.show();
+//                 }}
+//             >
+//                 {balance?.balance.toFixed(2) || "300"}{" "}
+//                 {balance?.currency || "$"}
+//             </button>
+
+//             <dialog
+//                 ref={dialogRef}
+//                 className="absolute left-1/2 top-full z-10 min-w-[8rem] -translate-x-1/2 overflow-hidden rounded-md bg-[#2c2d30] text-white shadow-md outline-none"
+//             >
+//                 <ul className="text-left text-sm">
+//                     <li className="w-max px-2.5 py-2 font-semibold">
+//                         Баланс {balance?.balance.toFixed(2)} {balance?.currency}
+//                     </li>
+//                     <hr
+//                         // style={{ all: "unset" }}
+//                         className="border-transparent"
+//                     />
+//                     <li
+//                         onClick={() => {
+//                             dialogRef.current?.show();
+//                         }}
+//                         className="rounded-sm bg-[#1b1c1d] px-2.5 py-2 transition-colors duration-150 mh:hover:bg-slate-100 mh:hover:text-slate-900"
+//                     >
+//                         <Link to="/payment/replenishment">Пополнить</Link>
+//                     </li>
+//                     <hr className="border-transparent" />
+
+//                     <li
+//                         onClick={() => {
+//                             dialogRef.current?.show();
+//                         }}
+//                         className="rounded-sm bg-[#1b1c1d] px-2.5 py-2 transition-colors duration-150 mh:hover:bg-slate-100 mh:hover:text-slate-900"
+//                     >
+//                         <Link to="/payment/draw">Вывести</Link>
+//                     </li>
+//                 </ul>
+//             </dialog>
+//         </div>
+//     );
+// };
+
+// interface DropDownMenuItemProps extends React.LiHTMLAttributes {
+
+// }
+
+// const DropDownMenuItem: React.FC<DropDownMenuItemProps> = () => {
+
+// }
--- src/components/dropdown-menus/burger-menu.tsx
+++ src/components/dropdown-menus/burger-menu.tsx
@@ -84,13 +84,7 @@ export const BurgerMenu = () => {
                 open={burgerMenuOpen}
                 onOpenChange={setBurgerMenuOpen}
             >
-                <DropdownMenuTrigger
-                    // onClick={() => {
-                    //     if (burgerMenuOpen) setBurgerMenuOpen(false);
-                    //     else setBurgerMenuOpen(true);
-                    // }}
-                    className="rounded-full border border-[#414148] bg-[#252528] px-3 py-0.5"
-                >
+                <DropdownMenuTrigger className="rounded-full border border-[#414148] bg-[#252528] px-3 py-0.5">
                     <FiMenu />
                 </DropdownMenuTrigger>
 
