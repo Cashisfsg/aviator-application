776a9803f31c36c5994012138467faef9632afdb
Cashisfsg <cashisfsg@gmail.com>
2024-06-07 14:26:43 +0300

    fix supprot button width

--- src/pages/payment/components/technical-support.tsx
+++ src/pages/payment/components/technical-support.tsx
@@ -9,10 +9,12 @@ export const TechnicalSupport = () => {
         <a
             href={links?.support}
             target="_blank"
-            className="absolute right-5 top-[80%] flex items-center gap-x-2 rounded-lg border bg-slate-100/90 px-3 py-2 shadow-md transition-transform duration-200 mh:hover:scale-110"
+            className="group absolute right-5 top-[80%] flex items-center gap-x-2 rounded-lg border bg-slate-100/90 px-3 py-2 shadow-md transition-transform duration-200 mh:hover:scale-110"
         >
             <FaPhoneVolume />
-            <span className="sr-only sm:not-sr-only">Поддержка 24/7</span>
+            <span className="w-0 overflow-hidden whitespace-nowrap transition-all duration-150 mh:group-hover:w-32">
+                Поддержка 24/7
+            </span>
             <span className="inline-block h-2 w-2 rounded-full bg-[#36ca12]"></span>
         </a>
     );
